18e498e9ad42642dcce8d072e5c4f3b8d944ae50
Hans Vos <hans.vos@codeimmersives.com>
2020-04-07 12:25:07 -0400

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import faker from 'faker';
+import SingleFeed from './SingleFeed';
+import SingleCardRequest from './SingleCardRequest';
+
+class App extends Component{
+    
+//    render(){
+//        return (
+           
+//        )
+//    } 
+
+}
+
+
+
+
+export default App;
